998dcd932b601694a300c7a2e507428d2af3c431
jaydeep <jaydeep.vaghela@cmarix.com>
2025-03-28 11:19:46 +0530

    Set the design of the dashboard below section

---
+++ src/app/risk-dashboard/components/dashboard/components/volatility-chart/volatility-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-volatility-chart',
+  imports: [NgbTooltipModule],
+  templateUrl: './volatility-chart.component.html',
+  styleUrl: './volatility-chart.component.scss'
+})
+export class VolatilityChartComponent {
+
+}
