567e40ae8b0fa48f0fa139f423cf3d853d0c1fe3
jaydeepvaghela <jaydeep.vaghela@cmarix.com>
2025-04-11 16:11:37 +0530

    Fixed the error found in chromatic

--- src/app/risk-dashboard/components/sidebar/sidebar.stories.ts
+++ src/app/risk-dashboard/components/sidebar/sidebar.stories.ts
@@ -1,13 +1,36 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { SidebarComponent } from './sidebar.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 const meta: Meta<SidebarComponent> = {
   title: 'Components/Sidebar',
   component: SidebarComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [SidebarComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            queryParams: of({}),
+            snapshot: {
+              paramMap: {
+                get: () => null,
+              },
+              queryParamMap: {
+                get: () => null,
+              },
+            },
+          },
+        },
+      ],
+    }),
+  ],
 };
 
 export default meta;
-
 type Story = StoryObj<SidebarComponent>;
 
 export const Default: Story = {};
