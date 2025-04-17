2e8c37a02b0815d04f1065df55014c59d54630a9
jaydeepvaghela <jaydeep.vaghela@cmarix.com>
2025-04-17 19:08:04 +0530

    fixed the chromatic error of dashboard activatedRoute

--- src/app/risk-dashboard/components/dashboard/dashboard.stories.ts
+++ src/app/risk-dashboard/components/dashboard/dashboard.stories.ts
@@ -1,9 +1,32 @@
-import { Meta, StoryObj } from "@storybook/angular";
+import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 import { DashboardComponent } from "./dashboard.component";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
 
 const meta: Meta<DashboardComponent> = {
     title: 'Components/Risk-Manager/Dashboard',
     component: DashboardComponent,
+    decorators: [
+        moduleMetadata({
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: of({}),
+                        queryParams: of({}),
+                        snapshot: {
+                            paramMap: {
+                                get: () => null,
+                            },
+                            queryParamMap: {
+                                get: () => null,
+                            },
+                        },
+                    },
+                },
+            ],
+        }),
+    ],
 };
 export default meta;
 type Story = StoryObj<DashboardComponent>;
