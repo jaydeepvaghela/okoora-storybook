c34c481a35eb3df7f58d26e5d2857a47670e56e3
jaydeepvaghela <jaydeep.vaghela@cmarix.com>
2025-05-05 12:09:29 +0530

    Added kyc-main component for signup flow

---
+++ src/app/sign-up/kyc-main/kyc-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kyc-main',
+  imports: [],
+  templateUrl: './kyc-main.component.html',
+  styleUrl: './kyc-main.component.scss'
+})
+export class KycMainComponent {
+
+}
