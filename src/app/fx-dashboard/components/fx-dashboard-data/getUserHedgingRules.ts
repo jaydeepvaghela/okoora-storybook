7d9dbb5beadf8df78df065f994c8e529f97b2736
jaydeep <jaydeep.vaghela@cmarix.com>
2025-10-08 17:09:07 +0530

    Set cashflow exposure json object

--- src/app/fx-dashboard/components/fx-dashboard-data/getUserHedgingRules.ts
+++ src/app/fx-dashboard/components/fx-dashboard-data/getUserHedgingRules.ts
@@ -1,11 +1,11 @@
 
 
-export const  getUserHedgingRules = [
+export const getUserHedgingRules = [
     {
-        "id": "3101df35-6a51-4d1c-b7ff-bfd64f6780cd",
-        "accountId": "c3aefa63-d081-4a68-979a-bf666e576c18",
-        "importExosureType": "invoice",
-        "invoiceBillMinExposureAmount": 1.00,
+        "id": "d298525c-a984-4ca5-aaf2-2dcd1d106654",
+        "accountId": "c2213112-f2ef-4202-9f6a-db147e95b6c0",
+        "importExosureType": "cashflow",
+        "invoiceBillMinExposureAmount": 1,
         "invoiceBillMaxExposureAmount": 200000000.00,
         "invoiceBillMaxDuePeriod": "3 Months",
         "invoiceBillCurrencies": [
@@ -15,15 +15,49 @@ export const  getUserHedgingRules = [
             "USD",
             "JPY"
         ],
-        "invoiceBillBlacklist": [],
+        "invoiceBillBlacklist": [
+            {
+                "counterpartyId": "ebc53106-9026-44f4-934d-db83569d2ccf",
+                "counterpartyType": "customers",
+                "counterpartyName": "test"
+            },
+            {
+                "counterpartyId": "46ddf536-419a-4090-8797-650b48f106fe",
+                "counterpartyType": "customers",
+                "counterpartyName": "testuser"
+            }
+        ],
         "cashflowMinExposureAmount": null,
         "cashflowMaxExposureAmount": null,
         "cashflowMaxDuePeriod": null,
         "cashflowCurrencies": [],
         "ruleType": "Invoice",
-        "createdAt": "2025-09-24T10:57:44.7834918",
-        "updatedAt": "2025-09-24T10:57:44.7834928",
+        "createdAt": "2025-09-18T11:27:20.0483362",
+        "updatedAt": "2025-09-22T06:55:33.4579997",
         "isFromCashFlowAPI": false,
+        "directionType": 3
+    },
+    {
+        "id": "7e29d8ef-c4a8-4833-b384-3f8d5185c1a1",
+        "accountId": "c2213112-f2ef-4202-9f6a-db147e95b6c0",
+        "importExosureType": "cashflow",
+        "invoiceBillMinExposureAmount": null,
+        "invoiceBillMaxExposureAmount": null,
+        "invoiceBillMaxDuePeriod": null,
+        "invoiceBillCurrencies": [],
+        "invoiceBillBlacklist": [],
+        "cashflowMinExposureAmount": 5572,
+        "cashflowMaxExposureAmount": 74320,
+        "cashflowMaxDuePeriod": "1 Month",
+        "cashflowCurrencies": [
+            "GBP",
+            "CHF",
+            "JPY"
+        ],
+        "ruleType": "Cashflow",
+        "createdAt": "2025-09-18T11:27:20.0649947",
+        "updatedAt": "2025-09-18T11:27:20.0649951",
+        "isFromCashFlowAPI": true,
         "directionType": 3
     }
 ]
